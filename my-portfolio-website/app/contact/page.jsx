0cfd73fbd9a4fc8d3e48e65baaf2ec72fd72782f
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-21 20:37:46 +0530

    update page

--- my-portfolio-website/app/contact/page.jsx
+++ my-portfolio-website/app/contact/page.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none gap-5">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-cyan-300">Let's work together</h3>
               <p className="text-white/60">
