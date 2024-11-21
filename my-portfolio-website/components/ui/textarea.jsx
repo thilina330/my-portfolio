fb2fca3be29a92efaa14346b6205806750cc43b3
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-11-21 20:38:49 +0530

    update teatarea

--- my-portfolio-website/components/ui/textarea.jsx
+++ my-portfolio-website/components/ui/textarea.jsx
@@ -1,18 +1,19 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
-    (<textarea
+    <textarea
       className={cn(
         "flex min-h-[80px] rounded-md border border-white/10 bg-primary px-4 py-5 text-base placeholder:text-white/60  focus-visible:outline-none focus-visible:ring-1 focus-wisible:ring-1 focus-visible:ring-cyan-300 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       ref={ref}
-      {...props} />)
+      {...props}
+    />
   );
-})
-Textarea.displayName = "Textarea"
+});
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
